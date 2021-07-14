53fcb03831c5a09485d0ac4dd9cf249b6aed5e4f
Leonardo2506 <maggical2014.lf@gmail.com>
2021-07-14 15:45:29 -0300

    Update Bolillero.cs

--- Bolillero.Core/Bolillero.cs
+++ Bolillero.Core/Bolillero.cs
@@ -1,9 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace Bolillero.Core
 {
     public class Bolillero
     {
-        
+        byte Cantidad {get; set;}
+        List<byte> Afuera { get; set;}
+        List<byte> Adentro {get; set;}
+        public Bolillero()
+        {
+            Afuera = new List<byte>{};
+            Adentro = new List<byte>{};
+        }
+        public Bolillero(byte Cantidad)
+        {
+            for(byte i=0; i < Cantidad; i++)
+            {
+                
+            }
+        }
+        public byte IngresarBolilla()
+        {
+            
+        }
+        public byte SacarBolilla()
+        {
+
+        } 
     }
+
+    
 }
