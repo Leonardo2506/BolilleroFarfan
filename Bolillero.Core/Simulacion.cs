5d9c6fc3e91393a0c6a9b998acb8257950fdf2f8
Leonardo2506 <maggical2014.lf@gmail.com>
2021-10-14 15:51:26 -0300

    ConfigurarVectorTareas

--- Bolillero.Core/Simulacion.cs
+++ Bolillero.Core/Simulacion.cs
@@ -15,24 +15,22 @@ namespace Bolillero.Core
         public long simularConHilos(Bolillero bolillero, long jugarNVeces, List<byte> jugada, int CantidadHilos)
         {
             
-            var vector = new Task<long>[CantidadHilos]; 
-
-            long simulacionPorHilos = jugarNVeces/CantidadHilos;
-
-            for(int i=0; i < CantidadHilos; i++)
-            {
-                var clone = (Bolillero)bolillero.Clone();  
-
-                vector[i] = Task<long>.Run(() => clone.jugarNVeces(jugada, simulacionPorHilos)); 
-            }
+            var vector = ConfigurarVectorTareas(bolillero, jugarNVeces, jugada, CantidadHilos);
 
             Task<long>.WaitAll(vector);
             
             return vector.Sum(t => t.Result); 
         }
 
-        
         public async Task<long> simularConHilosAsync(Bolillero bolillero, long jugarNVeces, List<byte> jugada, int CantidadHilos)
+        {
+            var vector = ConfigurarVectorTareas(bolillero, jugarNVeces, jugada, CantidadHilos);
+            await Task.WhenAll(vector);
+
+            return vector.Sum(t => t.Result); 
+        }
+
+        private Task<long>[] ConfigurarVectorTareas(Bolillero bolillero, long jugarNVeces, List<byte> jugada, int CantidadHilos)
         {
             var vector = new Task<long>[CantidadHilos]; 
 
@@ -43,11 +41,9 @@ namespace Bolillero.Core
                 var clone = (Bolillero)bolillero.Clone();  
                 
                 vector[i] = Task<long>.Run(() => clone.jugarNVeces(jugada, simulacionPorHilos)); 
-            }
-
-            await Task.WhenAll(vector);
-
-            return vector.Sum(t => t.Result); 
+            }   
+            
+            return vector; 
         }
     }
 }
